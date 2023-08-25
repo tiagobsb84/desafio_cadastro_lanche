d208fc1f32f2b612ae831122e30a453015af2cd5
Tiago Silva <tiagobsb84@gmail.com>
2023-08-24 23:27:16 -0300

    adicionado metodo lista todos pedidos

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid');
+const port = 3001;
+
+app.use(express.json());
+
+const listaPedidos = [];
+
+app.get('/pedidos', (request, response) => {
+    return response.json(listaPedidos);
+})
+
+app.listen(port, () => {
+    console.log(`Porta: ${port}`);
+});
